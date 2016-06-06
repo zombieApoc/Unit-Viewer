6af4f7ee57237b184a5f173f91c7ab2d8322f468
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-06-06 13:41:07 -0400

    Setting up project file

---
+++ src/main/java/com/wh40k/unitviewer/UnitViewerApplication.java
@@ -0,0 +1,12 @@
+package com.wh40k.unitviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitViewerApplication.class, args);
+	}
+}
